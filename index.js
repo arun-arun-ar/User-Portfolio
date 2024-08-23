522ab9a458e09a95623a2d93e29d8c830f4f6081
arun-arun-ar <arunacharya2056@gmail.com>
2024-08-23 15:26:01 +0545

    almost all completed

--- index.js
+++ index.js
@@ -16,4 +16,26 @@
         // Display the corresponding content section
         document.getElementById(tabname).classList.add("display-skills");
     }
+    document.addEventListener('DOMContentLoaded', function() {
+        const scriptURL = 'https://script.google.com/macros/s/AKfycbx_SmWiL8xhJ6yG0h9OwnxRQCfkFV2wM2jESPDoCru_kqClFg5jbl9wrUnHTdwgzNdn/exec'
+        const form = document.querySelector('form')
 
+        if (form) {
+            form.addEventListener('submit', e => {
+                e.preventDefault()
+                alert('Your message is being submitted. Please wait...')
+                fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+                    .then(response => {
+                        console.log('Success!', response)
+                        alert('Your message has been submitted to the form.')
+                        form.reset() // Clear the form
+                    })
+                    .catch(error => {
+                        console.error('Error!', error.message)
+                        alert('An error occurred. Please try again.')
+                    })
+            })
+        } else {
+            console.error('Form not found')
+        }
+    })
